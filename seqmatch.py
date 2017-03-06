39beeecc311117036e2334323d0ba1d27281d542
Mackenna Galicia <mgalicia@ganesh.hpc.genomecenter.ucdavis.edu>
2017-03-06 13:51:18 -0800

    Updated target, query

--- seqmatch.py
+++ seqmatch.py
@@ -8,8 +8,8 @@ from datetime import datetime
 from subprocess import Popen
 
 bevelPath = "/share/biocore/internal_projects/seqmatch/bevel/bin/bevel"
-targetDB = "/share/biocore/internal_projects/seqmatch/genomes/573.1728/573.1728.fna"
-queryDB = "/share/biocore/internal_projects/seqmatch/03-SpadesAssemblies/23_S128/23_S128.Scaffolds.fna"
+targetDB = "/share/biocore/internal_projects/seqmatch/genomes/59814.6/59814.6.fna"
+queryDB = "/share/biocore/internal_projects/seqmatch/03-SpadesAssemblies/26-3_S36/26-3_S36.Scaffolds.fna"
 outputFile = "output_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
 
 p = Popen([bevelPath, targetDB, queryDB])
@@ -19,6 +19,4 @@ p = Popen([bevelPath, targetDB, queryDB])
 p.communicate()
 if p.returncode != 0:
 	raise OSError('bevel error')
-
-
 
